e7fa1ee9a0d6684529f3c0d7071678072d8a8eb8
MALLE-PC\Malin <malin.thelin12@gmail.com>
2018-02-13 14:44:35 +0100

    remove unused code in test deform manager

--- Assets/Scripts/Test_DeformManager.cs
+++ Assets/Scripts/Test_DeformManager.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test_DeformManager : MonoBehaviour {
+public class Test_DeformManager : MonoBehaviour
+{
 
     public Material garmentMaterial;
     public DeformManager deformManager;
@@ -22,29 +23,9 @@ public class Test_DeformManager : MonoBehaviour {
         testDeformCloth.size = new Vector2(2, 3);
         testDeformCloth.material = garmentMaterial;
 
-
-
-        //go.AddComponent<DeformCloth>().testDeformCloth as DeformCloth;
-        
-
         DeformCloth dc = go.AddComponent<DeformCloth>();
         dc.SetSize(2, 3);
         dc.SetMaterial(garmentMaterial);
-//        go.GetComponent<DeformCloth>().SetSize(2,3);
-
-
-
-        //dc.size = new Vector2(3,3);
-        //dc.material = garmentMaterial;
-        
-    
-        //go.transform.parent = transform;
-        //go.transform.position = transform.position;
-
-        //Instantiate(go, new Vector3(0, 2, 0), Quaternion.identity);
-
-//        GameObject.Destroy(go);
-  //      go = null;
 
         deformManager.Reset();
 
