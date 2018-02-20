80e9789c240a5d7f942ba39abb6ad4122ae6bc26
MALLE-PC\Malin <malin.thelin12@gmail.com>
2018-02-20 17:23:18 +0100

    sprites and a test ui created

---
+++ Assets/Scripts/SimpleToggleButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class SimpleToggleButton : MonoBehaviour
+
+{
+
+
+    bool pressed = true;
+
+    void OnGUI()
+
+    {
+        pressed = GUILayout.Toggle(pressed, "Toggle me !", "Button");
+        
+    }
+
+}
