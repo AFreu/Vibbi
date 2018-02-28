efad0ee686326847d0a444a15a269d50e01adc8e
Anton Freudenthaler <anton.p.v.freudenthaler@gmail.com>
2018-02-28 13:17:32 +0100

    Added Attachment Points to Vibbi. Its possible to add them to a object in layer 'body'.

---
+++ Assets/Scripts/AttachmentPoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachmentPoint : MonoBehaviour {
+
+	public GameObject body;
+
+	private Vector3 closestPointOnBody;
+
+	// Use this for initialization
+	void Start () {
+		closestPointOnBody = body.GetComponent<Collider> ().ClosestPoint (transform.position);
+
+		Debug.DrawLine (closestPointOnBody, transform.position, Color.blue, 100);
+
+		Vector3 v = transform.position - closestPointOnBody;
+		Vector3 normal = v.normalized;
+
+		transform.up = normal;
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
