4aec01d9253e0d0dcf9456877a721dab514dc1ba
Anton Freudenthaler <anton.p.v.freudenthaler@gmail.com>
2018-03-12 17:07:15 +0100

    SeamBehaviour draw lines between seam edges

--- Assets/Scripts/SeamBehaviour.cs
+++ Assets/Scripts/SeamBehaviour.cs
@@ -7,16 +7,65 @@ public class SeamBehaviour : MonoBehaviour {
 	public GameObject lineOne;
 	public GameObject lineTwo;
 
+	private List<Transform> startPoints = new List<Transform> ();
+	private List<Transform> endPoints = new List<Transform> ();
+
+	private List<GameObject> connections = new List<GameObject>();
+
 
 	void Start(){
 	}
 
 	void Update(){
+
+		if (Input.GetKeyUp (KeyCode.W)) {
+			Swap ();
+		}
+
+		if (lineOne == null || lineTwo == null)
+			return;
+
+		UpdateConnections ();
+
+	}
+
+	void UpdateConnections(){
+		for(int i = 0; i < connections.Count; i++) {
+			var lineRenderer = connections[i].GetComponent<LineRenderer> ();
+			lineRenderer.SetPosition (0, startPoints[i].position);
+			lineRenderer.SetPosition (1, endPoints[i].position);
+		}
+	}
+
+	void AddConnection(Transform start, Transform end){
+
+		startPoints.Add (start);
+		endPoints.Add (end);
+		connections.Add(VibbiUtils.CreateLine (start.position, end.position, Color.blue, transform));
+
+	}
+
+	void OnDestroy(){
+		foreach (GameObject c in connections) {
+			Destroy (c);
+		}
 	}
 
 	public void Init(GameObject line1, GameObject line2){
 		lineOne = line1;
 		lineTwo = line2;
+
+		var p1 = lineOne.GetComponent<BoundaryLineBehaviour> ().first;
+		var p2 = lineTwo.GetComponent<BoundaryLineBehaviour> ().first;
+		var p3 = lineOne.GetComponent<BoundaryLineBehaviour> ().second;
+		var p4 = lineTwo.GetComponent<BoundaryLineBehaviour> ().second;
+
+		AddConnection (p1, p2);
+		AddConnection (p3, p4);
+	}
+
+	public void Swap(){
+		endPoints.Reverse ();
 	}
 
 	public GameObject GetFirstLine(){
---
+++ Assets/Scripts/VibbiUtils.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VibbiUtils : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.1f)
+	{
+		GameObject myLine = new GameObject();
+		myLine.transform.position = start;
+		myLine.AddComponent<LineRenderer>();
+		LineRenderer lr = myLine.GetComponent<LineRenderer>();
+		lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+		lr.startColor = color;
+		lr.endColor = color;
+		lr.startWidth = 0.1f;
+		lr.endWidth = 0.1f;
+		GameObject.Destroy(myLine, duration);
+	}
+
+	public static GameObject CreateLine(Vector3 start, Vector3 end, Color color, Transform parent){
+		GameObject myLine = new GameObject("Connection");
+		myLine.transform.position = start;
+		myLine.transform.parent = parent;
+		myLine.AddComponent<LineRenderer>();
+		LineRenderer lr = myLine.GetComponent<LineRenderer>();
+		lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+		lr.startColor = color;
+		lr.endColor = color;
+		lr.startWidth = 0.1f;
+		lr.endWidth = 0.1f;
+		lr.SetPosition(0, start);
+		lr.SetPosition(1, end);
+		return myLine;
+	}
+
+}
